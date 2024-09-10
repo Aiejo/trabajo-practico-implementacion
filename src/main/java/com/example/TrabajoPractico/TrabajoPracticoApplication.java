11339bf3061102daa4a23c6fbfa21807584ee9e4
Alejandroch <alejandro9.ch321@gmail.com>
2024-09-09 22:42:06 -0500

    Finally works the docker

--- src/main/java/com/example/TrabajoPractico/TrabajoPracticoApplication.java
+++ src/main/java/com/example/TrabajoPractico/TrabajoPracticoApplication.java
@@ -2,11 +2,18 @@ package com.example.TrabajoPractico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TrabajoPracticoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("CONNECTION_URL", dotenv.get("CONNECTION_URL"));
+		System.out.println(dotenv.get("CONNECTION_URL"));
+		System.setProperty("USER", dotenv.get("USER"));
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
+		System.out.println(dotenv.get("CONNECTION_URL"));
 		SpringApplication.run(TrabajoPracticoApplication.class, args);
 	}
 
